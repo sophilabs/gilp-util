4a2e376ab245232a727ec7197431d54cab991f9d
Pablo Ricco <pricco@gmail.com>
2016-11-15 13:47:55 -0300

    Fix path to call local gulp.

--- templates/hook.js
+++ templates/hook.js
@@ -3,13 +3,17 @@ var execSync = require('child_process').execSync;
 var spawn = require('child_process').spawn;
 var path = require('path');
 var util = require('util');
+var env = process.env;
+
+env.PATH = execSync('npm bin').toString().trim() + ':' + env.PATH;
 
 var gulp = spawn(
-  path.join(execSync('npm bin gulp').toString().trim(), 'gulp'),
+  'gulp',
   ['gilp-%s'].concat(process.argv.slice(2).map(function (arg) { return util.format('--gilp-args=%%s', arg); })),
   {
     stdio: 'inherit',
-    cwd: process.env.PWD || process.cwd()
+    cwd: process.env.PWD || process.cwd(),
+    env: env
   }
 );
 
