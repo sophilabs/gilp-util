9ad872a5234db2f27bcb4f5c880b1b56619dc03f
Daniel de Amilivia <ddeamilivia@sophilabs.com>
2017-07-21 11:57:30 -0300

    Use Shell instead of Node

--- templates/hook.js
+++ templates/hook.js
@@ -1,22 +1,2 @@
-#!/usr/bin/env node
-var execSync = require('child_process').execSync;
-var spawn = require('child_process').spawn;
-var path = require('path');
-var util = require('util');
-var env = process.env;
-
-env.PATH = execSync('npm bin').toString().trim() + ':' + env.PATH;
-
-var gulp = spawn(
-  'gulp',
-  ['gilp-%s'].concat(process.argv.slice(2).map(function (arg) { return util.format('--gilp-args=%%s', arg); })),
-  {
-    stdio: 'inherit',
-    cwd: process.env.PWD || process.cwd(),
-    env: env
-  }
-);
-
-gulp.on('close', function (code) {
-  process.exit(code);
-});
+#!/usr/bin/sh
+`npm bin gulp`/gulp gilp-%s --gilp-args=$1
